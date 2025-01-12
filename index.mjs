e500dc0dba947f883dc4a53cf81c434b125082d5
prabowo-sakti <prabowosakti2023@gmail.com>
2025-01-12 09:17:29 +0700

    perubahan docker compose, belajar .env, belajar docker

--- index.mjs
+++ index.mjs
@@ -1,7 +1,14 @@
+import dotenv from 'dotenv'
 import { app } from "./server.mjs";
+
+dotenv.config()
+
+const mySecret  = process.env.MY_SECRET
+console.log(mySecret)
 
 const port = 3000;
 
 app.listen(port, () => {
   console.log(`Running in http://localhost:${port}`);
 });
+
